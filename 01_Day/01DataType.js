4804e1424a6c46c78fdb6dd7ce32dc1e97b4e455
Sweta Kunwar <swetakunwar22@gmail.com>
2023-07-17 15:40:34 +0530

    01_Q1

--- 01_Day/01DataType.js
+++ 01_Day/01DataType.js
@@ -1,2 +1,46 @@
 console.log("Sweta Kunwar")
 console.log("This is my first tutorial")
+/**
+ Data types
+ number
+ string
+ boolean
+ null
+ undefined
+ bigInt
+
+ To check the data type of the variable, we  use size of function.
+
+
+ Three types of key that help to define a varibale:
+ const
+ let
+ var
+
+ To defie a variable: key word,  name of the variable, (=) variables value
+
+ camel case: isEligibleSweta ( like camel hunge)
+
+ */
+let age = 22;
+let  uName = "sweta";
+let isEligibleSweta = true;
+console.log("Type of",age,typeof(age))
+console.log("Type of",uName,typeof(uName))
+console.log("Type of",isEligibleSweta,typeof(isEligibleSweta))
+
+
+
+
+let hair; 
+var salary=null;
+const sibling=3
+console.log(typeof(hair), typeof(salary), typeof(sibling))
+
+
+
+
+
+
+
+
--- 01_Day/01_Q1.js
+++ 01_Day/01_Q1.js
@@ -8,11 +8,22 @@
 */
 
 // var1
+var coutry= 'india';
+console.log(typeof(coutry))
 
 // var2
+const marks=98;
+console.log(typeof(marks))
 
 // var 3
+const table=null;
+console.log(typeof(table))
 
 // var 4
+var stars;
+console.log(typeof(stars))
+
 
 // var 5
+const isValid= true;
+console.log(typeof(isValid))
