302b4886695fa4c7c2e3c036bc661c49630cfe59
pramdevgan <vpramatma@gmail.com>
2023-07-17 15:24:09 +0530

    Question 1 (Data Types)

---
+++ 01_Day/01_Q1.js
@@ -0,0 +1,18 @@
+// Question
+
+/*
+    Declare 5 Varibale, using any type of keywords.
+    And There name should be meaningfull.
+
+    and check there datatype, with help of log 
+*/
+
+// var1
+
+// var2
+
+// var 3
+
+// var 4
+
+// var 5
